f9f441512d337fe8edd897964da6634b376e6048
Subrata K. Bhowmik <sdotbhowmik@gmail.com>
2025-01-12 18:49:51 +0600

    initial commit

---
+++ var-dt_practice.js
@@ -0,0 +1,42 @@
+var age = 60
+var age2 = 20
+var ageSum = age / age2
+console.log(ageSum)
+
+let sentence1 = "Hello words";
+let sentence2 = "I am learning Javascripts"
+console.log(sentence1 + " " + sentence2)
+
+var fruit1 = "apple"
+var fruit2 = "banana"
+var fruit3 = "apple"
+
+var isFruit = (fruit1 == fruit3)
+console.log(isFruit)
+
+let x
+let y = 20
+let z = null
+console.log(x)
+
+var emp_info_obj ={
+    name:"Jhon",
+    age:30,
+    isMarried:true,
+    address:"Dhaka",
+    phone:"01711111111",
+    email:"jhon@gmail.com",
+    skills:["html","css","Javascript","PHP"]
+
+}
+
+console.log(emp_info_obj.name)
+console.log(emp_info_obj.address," ", emp_info_obj.skills[1])
+console.log(emp_info_obj.isMarried)
+
+name = "abc"
+let plants = ["Rose", "Sunflower", "Tulip", "Lily","Gladiolus"]
+console.log(plants)
+console.log(typeof(plants))
+console.log(typeof(name))
+console.log(name)
